cd3875aff813b4002e97d15c4687b25b6e865e26
Syugen <syugen.zhou@gmail.com>
2016-10-23 12:24:17 -0400

    useless commit

--- test/index.php
+++ test/index.php
@@ -1,47 +1,35 @@
 <!DOCTYPE HTML">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>UTop Tutor Test</title>
-</head>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>UTop Tutor Test</title>
+    </head>
 
-<body>
-<?php
-/*	if(isset($_COOKIE["csc108test"])) {
-		echo "您已在过去的24小时（1日）内免费测试过，请等待或付费。<br>";
-	} else {
-		echo "请注意，每24小时（1日）内您只能免费测试一次，获得更多测试机会我们提供付费服务。<br>";
-	}
-*/	setcookie("csc108test_index", "csc108test_index", time()+86400);
-?>
-<!--点击<a href="pay" target="_blank">这里</a>查看具体付费信息。<br>-->
-<h3>CSC108 Assignment 2免费测试</h3>
-<script src="//www.powr.io/powr.js" external-type="html"></script> 
-<form name="form" id="form" method="post" action="result.php" enctype="multipart/form-data">
-<?php
-	if(isset($_COOKIE["csc108test"])) {
-		echo "请输入付费号码：<input name='uid' type='text'><br>";
-	} else {
-		echo "<input name='uid' type='hidden'>";
-	}
-?>
-<table>
-<tr>
-	<td>上传palindromes.py:</td>
-	<td><input type="file" name="palindromes" id="palindromes"></td>
-</tr>
-<tr>
-	<td>上传dna.py:</td>
-	<td><input type="file" name="dna" id="dna"></td>
-</table>
-	文件名必须为"pylindromes.py"和"dna.py"，否则会编译失败。<br>
-	说明：点击提交按钮，表明你已知晓你的代码将会加密传输给utoptutoring.ml服务器；<br>
-	服务器在测试完你的代码后会立刻（在你看到结果之前）删除你传送的文件。<br>
-	<input name="submit" type="submit" value="提交">
-<br><br><br>课程辅导报名网址：<a href="../index.php">utoptutoring.ml</a>
-</form>
-<img src="http://hitwebcounter.com/counter/counter.php?page=6518574&style=0024&nbdigits=6&type=ip&initCount=0" title="" Alt=""   border="0" >
-<br>
-<img src="http://hitwebcounter.com/counter/counter.php?page=6518573&style=0024&nbdigits=6&type=page&initCount=0" title="" Alt=""   border="0" >
-</body>
+    <body>
+        <?php
+            // csc108test_index是测试页面未刷新的cookie
+            setcookie("csc108test_index", "csc108test_index", time()+86400);
+        ?>
+
+        <h3>CSC108 Assignment 2免费测试</h3>
+        <form name="form" id="form" method="post" action="result.php" enctype="multipart/form-data">
+            <table>
+                <tr>
+                    <td>上传palindromes.py:</td>
+                    <td><input type="file" name="palindromes" id="palindromes"></td>
+                </tr>
+                <tr>
+                    <td>上传dna.py:</td>
+                    <td><input type="file" name="dna" id="dna"></td>
+                </tr>
+            </table>
+            文件名必须为"pylindromes.py"和"dna.py"，否则会编译失败。<br>
+            说明：点击提交按钮，表明你已知晓你的代码将会加密传输给utoptutoring.ml服务器；<br>
+            服务器在测试完你的代码后会立刻（在你看到结果之前）删除你传送的文件。<br>
+            <input name="submit" type="submit" value="提交">
+        </form>
+        <p>课程辅导报名网址：<a href="../index.php">utoptutoring.ml<p>
+        <img src="http://hitwebcounter.com/counter/counter.php?page=6518574&style=0024&nbdigits=6&type=ip&initCount=0" title="" Alt="" border="0" ><br>
+        <img src="http://hitwebcounter.com/counter/counter.php?page=6518573&style=0024&nbdigits=6&type=page&initCount=0" title="" Alt="" border="0" >
+    </body>
 </html>
--- test/result.php
+++ test/result.php
@@ -1,67 +1,69 @@
 <!DOCTYPE HTML">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>UTop Tutor Test Result</title>
-</head>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>UTop Tutor Test Result</title>
+    </head>
 
-<body>
-<?php
-ini_set('display_errors', 1); error_reporting(E_ALL);
-	if(isset($_COOKIE["csc108test"])) {
-		echo "You have already submitted once in the last 24 hours.<br>";
-		echo "Please try again later.<br>";
-	} else if(!isset($_COOKIE["csc108test_index"]))
-		header("Location: ../404.html");
-	else {
-		setcookie("csc108test_index", "", time()-86400);
-$upload_fail = 0;
-if(isset($_FILES['dna'])){
-    $name_dna   = $_FILES['dna']['name'];
-    $temp_name  = $_FILES['dna']['tmp_name'];
-    if(!isset($name_dna) || $name_dna != "dna.py" || !move_uploaded_file($temp_name, $name_dna)){
-            echo 'Upload dna.py failed<br>';
-		$upload_fail = 1;
-    }
-};
-if(isset($_FILES['palindromes'])){
-    $name_pal   = $_FILES['palindromes']['name'];
-    $temp_name  = $_FILES['palindromes']['tmp_name'];
-    if(!isset($name_pal) || $name_pal != "palindromes.py" || !move_uploaded_file($temp_name, $name_pal)){
-            echo 'Upload palindromes.py failed<br>';
-		$upload_fail = 1;
-    }
-};
-if(!$upload_fail){
-		$file = fopen("ip.txt", "w") or die("Cannot open file.");
-		fwrite($file, $_SERVER["REMOTE_ADDR"]);
-		fclose($file);
-		exec("python3 a2_test.py 2> error.txt");
-		if(file_exists("test_result.txt")) {
-//			setcookie("csc108test", "csc108test", time()+86400);
-			$file = fopen("test_result.txt", "r");
-			$content = fread($file, filesize("test_result.txt"));
-			echo "<h2>Your result:</h2><code><pre>".$content."</pre></code>";
-			fclose($file);
-			unlink("test_result.txt");
-//			if(file_exists("error.txt")) unlink("error.txt");
-		} else {
-			$file = fopen("error.txt", "r");
-			$content = fread($file, filesize("error.txt"));
-			unlink("error.txt");
-			echo "<h2>你的代码无法编译</h2>";
-			echo "请确保代码中无语法错误再提交。<br>";
-//			echo "<br>You don't have to wait 24 hours to resubmit.<br>";
-			echo "错误信息如下：<br><code><pre>";
-			echo $content."</pre></code>";
-		}
-	}
-}
-unlink("dna.py");
-unlink("palindromes.py");
-unlink("ip.txt");
-?>
-<br><a href="./">返回重新测试</a>
-<br><br>课程辅导报名网址：<a href="../index.php">utoptutoring.ml</a>
-</body>
+    <body>
+    <?php
+        // 设置php debug显示选项，无错误时不显示；有错误时显示
+        ini_set("display_errors", 1); error_reporting(E_ALL);
+
+        // 检查网页刷新cookie，有则正常，并删除；没有则说明刷新过
+        if(!isset($_COOKIE["csc108test_index"]))
+            header("Location: ../404.html");
+        setcookie("csc108test_index", "", time()-86400);
+
+        // 上传文件，未上传、文件名错、创建文件出错则报错
+        $upload_fail = 0;
+        if(isset($_FILES["dna"])){
+            $name_dna  = $_FILES["dna"]["name"];
+            $temp_name = $_FILES["dna"]["tmp_name"];
+            if(!isset($name_dna) || $name_dna != "dna.py" || !move_uploaded_file($temp_name, $name_dna)){
+                echo "Upload dna.py failed<br>";
+                $upload_fail = 1;
+            }
+        }
+        if(isset($_FILES["palindromes"])){
+            $name_pal  = $_FILES["palindromes"]["name"];
+            $temp_name = $_FILES["palindromes"]["tmp_name"];
+            if(!isset($name_pal) || $name_pal != "palindromes.py" || !move_uploaded_file($temp_name, $name_pal)){
+                echo "Upload palindromes.py failed<br>";
+                $upload_fail = 1;
+            }
+        }
+
+        // 上传成功
+        if(!$upload_fail){
+            // 记录用户ip
+            $file = fopen("ip.txt", "w") or die("Cannot open file.");
+            fwrite($file, $_SERVER["REMOTE_ADDR"]);
+            fclose($file);
+
+            // python3测试
+            exec("python3 a2_test.py 2> error.txt");
+
+            // 运行成功（无编译错误）
+            if(file_exists("test_result.txt")) {
+                $file = fopen("test_result.txt", "r");
+                $content = fread($file, filesize("test_result.txt"));
+                echo "<h2>Your result:</h2><code><pre>".$content."</pre></code>";
+                fclose($file);
+                unlink("test_result.txt");
+            } else {
+                $file = fopen("error.txt", "r");
+                $content = fread($file, filesize("error.txt"));
+                unlink("error.txt");
+                echo "<h2>你的代码无法编译</h2>请确保代码中无语法错误再提交。<br>";
+                echo "错误信息如下：<br><code><pre>".$content."</pre></code>";
+            }
+        }
+        if(file_exists("dna.py")) unlink("dna.py");
+        if(file_exists("palindromes.py")) unlink("palindromes.py");
+        if(file_exists("ip.txt")) unlink("ip.txt");
+    ?>
+    <p><a href="./">返回重新测试</a></p>
+    <p>课程辅导报名网址：<a href="../index.php">utoptutoring.ml</a></p>
+    </body>
 </html>
